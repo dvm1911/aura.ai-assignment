83b78af7c7952f607ecee7e1eb3028e374ab50a1
Digvijay Singh <dig.malik1911@gmail.com>
2024-10-10 20:47:01 +0530

    final draft

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-const clientId = "453687433752-p0jqss0kno5dliieoh98bl1gvfp8euea.apps.googleusercontent.com"
+const clientId = "156424469641-d7fvkat6g4nc8brgctkvlb7j8v9ihaoo.apps.googleusercontent.com"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
